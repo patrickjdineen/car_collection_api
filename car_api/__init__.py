0aa705123847664e7c7a416d063b595417bcd0ad
Patrick Dineen <patrickjdineen@gmail.com>
2021-02-02 21:26:09 -0500

    updated

--- car_api/__init__.py
+++ car_api/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask
 #imports Config class defined in config.py takes information, secret key, etc 
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 #create the application from class type Flask
 app = Flask(__name__)
 app.config.from_object(Config)
